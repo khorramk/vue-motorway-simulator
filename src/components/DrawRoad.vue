afa12e4aed95a2de2c7e81f3763fcf41a7e5764d
Khorram Khondkar <khorramk.kbsk@gmail.com>
2024-01-18 16:16:02 +0000

    rawing lanes and other cars

--- src/components/DrawRoad.vue
+++ src/components/DrawRoad.vue
@@ -1,9 +1,21 @@
 <template>
   <RoadWay>
-    <LaneMarker></LaneMarker>
-    <OtherCar :speed="2" :car-color="'black'" :car-position-horizontal="30"></OtherCar>
-    <OtherCar :speed="2" :car-color="'purple'" :car-position-horizontal="60" ></OtherCar>
-    <OtherCar :speed="2" :car-color="'blue'" :car-position-horizontal="80"></OtherCar>
+    <LaneMarker lane-type="solid" mark-position="1rem"></LaneMarker>
+    <OtherCar :speed="2" :car-color="'black'" :car-position-horizontal="30" direction="bottom"></OtherCar>
+    <OtherCar :speed="2" :car-color="'purple'" :car-position-horizontal="60" direction="bottom"></OtherCar>
+    <OtherCar :speed="2" :car-color="'blue'" :car-position-horizontal="80" direction="bottom"></OtherCar>
+    <LaneMarker lane-type="dashed" mark-position="30%"></LaneMarker>
+    <LaneMarker lane-type="dashed" mark-position="70%"></LaneMarker>
+
+  </RoadWay>
+  <RoadWay>
+    <LaneMarker lane-type="solid" mark-position="1rem"></LaneMarker>
+    <OtherCar :speed="2" :car-color="'black'" :car-position-horizontal="30" direction="top" car-body="car-body"></OtherCar>
+    <OtherCar :speed="2" :car-color="'purple'" :car-position-horizontal="60" direction="top" car-body="car-body"></OtherCar>
+    <OtherCar :speed="2" :car-color="'blue'" :car-position-horizontal="80" direction="top" car-body="car-body"></OtherCar>
+    <LaneMarker lane-type="dashed" mark-position="30%"></LaneMarker>
+    <LaneMarker lane-type="dashed" mark-position="70%"></LaneMarker>
+
   </RoadWay>
 </template>
 
@@ -12,3 +24,9 @@ import RoadWay from './RoadWay.vue'
 import LaneMarker from './LaneMarker.vue'
 import OtherCar from './OtherCar.vue'
 </script>
+
+<style>
+.car-body {
+     border-radius: 20px 20px 40px 40px;
+}
+</style>
